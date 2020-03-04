04a92fabec2503fca48da10e2f21ef8efb7e64f2
Xiaoyi Liu <lxy878@gmail.com>
2020-03-04 23:02:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,13 @@ def line(s1)
   message = "The line is currently"
   if s1.size == 0
     puts "#{message} empty."
+  else
+    message << ":"
+    s1.length.times do |i|
+      message << " #{i}. #{s1[i]}"
+    end
   end
+  return messgage
 end
 
 def take_a_number(s1, s2)
